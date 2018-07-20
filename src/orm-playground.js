2ff64da70a33838445d205f5739409ff0dbe0de7
Joshua Anthony <jantho1990@gmail.com>
2018-07-19 22:34:59 -0500

    updated

--- src/orm-playground.js
+++ src/orm-playground.js
@@ -10,20 +10,30 @@ console.log('Session', session)
 
 const { Author, Book } = session
 
-Book.create({
+let nobodyCares = Book.create({
     name: 'The Hedegrast'
 })
+
+console.log('First Book', nobodyCares.name)
 
 let orson = Author.create({
     name: 'Orson Gott Gard'
 })
 
-Book.create({
+let ender = Book.create({
     name: 'Ender\'s Butt',
     'authors': [orson]
 })
 
 console.log('All Books', Book.all().toModelArray())
 
-console.log('Book 1 Title', Book.withId(1).name)
+console.log('Book 1 Title', ender.name)
 console.log('Book 1 Authors', Book.withId(1).authors.toModelArray()[0].name)
+
+ender.update({name: 'Ender\'s Shadow Realm'})
+
+console.log('Book 1 Updated Title', Book.withId(1).name)
+
+nobodyCares.delete()
+
+console.log('Nobody Cares', Book.withId(0))
